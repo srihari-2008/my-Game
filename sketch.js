01189acabe78a5227ccbcb5d689d14ebcbe85eb8
srihari-2008 <66555096+srihari-2008@users.noreply.github.com>
2020-09-13 09:34:55 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,13 +2,16 @@ var playerName;
 var gameState = "START"
 var input;
 var start,startimage,rocket1,rocket1image,rocket2,rocket2image;
-var player,launch,launchimage;
+var player,startrocket,launchimage;
+var a;
+var meteorsGroup;
 
 function preload(){
   startimage = loadImage("dark green start button.jpg")
   rocket1image = loadImage("rocket1.png")
-  rocket2image = loadImage("rocket2.jpg")
+  rocket2image = loadImage("rocket 2.png")
   launchimage = loadImage("green-start-button.jpg")
+  bgrimage = loadImage("bacground 2.jpg")
 }
 
 function setup() {
@@ -16,27 +19,32 @@ function setup() {
   start = createSprite(displayWidth/2, displayHeight/2 + 60, 1, 1);
   rocket1 = createSprite(200,400, 1, 1);
   rocket2 = createSprite(500,400, 1, 1);
-  launch = createSprite(250,800,1,1)
-   //launch.scale = 0.4
-  player = createSprite(300,400,1,1)
+  startrocket = createSprite(800,200,1,1);
+   startrocket.scale = 0.4
+  player = createSprite(300,400,1,1);
+  meteorsGroup = new Group();
 
   input = createInput();
 }
 
 function draw() {
   background(100,55,255);  
+  image(bgrimage,0,-displayHeight * 4,displayWidth,displayHeight * 5)
   if(gameState === "START"){
-    input.position(displayWidth/2 - 20,displayHeight/2 - 20)
+    input.position(displayWidth/2 - 20,displayHeight/2 - 20);
     playerName = input.value()
-    text("Hello"+playerName,displayWidth/2 + 10,displayHeight/2 + 20)
-    start.addImage(startimage)
+    text("Hello"+playerName,displayWidth/2 + 10,displayHeight/2 + 20);
+    start.addImage(startimage);
     start.scale = 0.2;
     if(mousePressedOver(start)){
       gameState = "screen1"
     }
 
+    
+
   }
 if(gameState === "screen1"){
+  image(bgrimage,0,-displayHeight * 4,displayWidth,displayHeight * 5)
   background(155,50,25)
   start.destroy();
   rocket1.addImage(rocket1image)
@@ -44,9 +52,9 @@ if(gameState === "screen1"){
   rocket2.addImage(rocket2image)
   rocket2.scale = 0.3
   input.hide();
-  launch.addImage("launch",launchimage)
-  launch.scale = 0.4
-  launch.visible = false;
+  startrocket.addImage(launchimage)
+  startrocket.scale = 0.3
+  startrocket.visible = false;
   if(mousePressedOver(rocket1)){
     gameState = "bhaskara1"
     
@@ -57,21 +65,51 @@ if(gameState === "screen1"){
   
 }
 if(gameState === "bhaskara1"){
+  image(bgrimage,0,-displayHeight * 4,displayWidth,displayHeight * 5)
   background(120,20,75)
   rocket1.destroy();
   rocket2.destroy();
   input.hide();
-  launch.visible = true
+  startrocket.visible = true
   player.addImage("player",rocket1image)
   player.scale = 0.3;
+  if(mousePressedOver(startrocket)){
+    player.velocityY = -5;
+    player.velocityX = 0;
+    a = 1
+  }
+if(a == 1){
+  if(keyDown(LEFT_ARROW)){
+    player.velocityX = -5;
+    player.velocityY = 0;
+  }
+
+  if(keyDown(RIGHT_ARROW)){
+    player.velocityX = 5;
+    player.velocityY = 0;
+  }
+}
+    if(frameCount % 70 === 0 && a === 1)  {
+      var meteors = createSprite(250,0,50,36)
+      meteors.x = random(0,400);
+      meteors.velocityY = 3
+      meteorsGroup.add(meteors);
+
+    }
+    if(meteorsGroup.isTouching(player)){
+      meteorsGroup.destroyEach();
+    }
+  
+  
 }
 
 if(gameState === "bhaskara2"){
+  image(bgrimage,0,-displayHeight * 4,displayWidth,displayHeight * 5)
   background(100,250,35)
   rocket1.destroy();
   rocket2.destroy();
   input.hide();
-  launch.visible = true
+  startrocket.visible = true
   player.addImage("player",rocket2image)
   player.scale = 0.3;
 }
