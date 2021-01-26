85a4d04b957c7bafdea4a9756701fa7ac073c489
srihari-2008 <66555096+srihari-2008@users.noreply.github.com>
2021-01-26 09:14:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,27 +4,24 @@ var input;
 var start,startimage,rocket1,rocket1image,rocket2,rocket2image;
 var player,startrocket,launchimage;
 var a;
-var meteorsGroup;
+var meteorsGroup,meteorsGroup2;
 var bgr;
 var score = 0;
 var bullet ;
 var meteorimage;
 var powerGroup;
 var b;
-var powerimage;
-
 
 function preload(){
   startimage = loadImage("dark green start button.jpg")
   rocket1image = loadImage("rocket1.png")
-  rocket2image = loadImage("rocket 2.png")
+  rocket2image = loadImage("rocket3.png")
   launchimage = loadImage("green-start-button.jpg")
   bgrimage = loadImage("bacground 2.jpg")
   meteorimage = loadImage("meteor.png")
   bgrimage2 = loadImage("bacground 1.jpg")
   asteroidimage = loadImage("asteroid2.png")
   alienimage = loadImage("alien.png")
-  powerimage = loadImage("power.jpg")
 }
 
 function setup() {
@@ -39,10 +36,9 @@ function setup() {
 
   powerGroup = new Group();
   meteorsGroup = new Group();
+  meteorsGroup2 = new Group();
   asteroidsGroup = new Group();
   alienGroup = new Group();
-
-  
   
 
   input = createInput();
@@ -76,41 +72,51 @@ if(gameState === "screen1"){
   startrocket.addImage(launchimage)
   startrocket.scale = 0.3
   startrocket.visible = false;
-  if(mousePressedOver(rocket1)){
-    gameState = "bhaskara1"
+  textSize(20)
+  text("Some rockets go faster than the speed of sound (Mach 1)",640,100)
+  text("and may reach speeds of up to 30'000 km/hr (Mach 25) when in orbit",640,120)
+  text("Most rockets still use solid fuel to make the fire. The biggest ones use liquid fuel",640,180);
+  text("because it makes a hotter fire so the rocket is more powerful.",640,200);
+  text("However, handling the liquid fuel safely is difficult and expensive.",640,220)
+
     
+    if(mousePressedOver(rocket1)){
+    gameState = "bhaskara1"
+    player.addImage("player",rocket1image)
+    bgr.addImage("bgr1",bgrimage)
+    bgr.visible=false
   }
   if(mousePressedOver(rocket2)){
-    gameState = "bhaskara2"
+    gameState = "bhaskara1"
+    player.addImage("player2",rocket2image)
+    bgr.addImage("bgr1",bgrimage2)
+    bgr.visible=false
   }
   drawSprites();  
 }
 if(gameState === "bhaskara1"){
-  image(bgrimage,0,-displayHeight * 4,displayWidth,displayHeight * 5)
+  //image(bgrimage,0,-displayHeight * 4,displayWidth,displayHeight * 5)
   background(120,20,75)
-  player.debug = true;
+ // player.debug = true;
   player.setCollider("rectangle",0,0,200,450)
   rocket1.destroy();
   rocket2.destroy();
   input.hide();
   startrocket.visible = true
-  player.addImage("player",rocket1image)
+ 
   player.scale = 0.3;
-  textSize(20)
-  text("This is bhaskara 1",900,600)
+  
   if(mousePressedOver(startrocket)){
-    bgr.addImage(bgrimage)
-   
-    bgr.scale = 3.2;
-    /*player.velocityY = -5;
-    player.velocityX = 0;*/
+    //bgr.addImage(bgrimage)
+   bgr.visible=true
+    bgr.scale = 4;
     a = 1
     startrocket.destroy();
     
   }
   
 if(a == 1){
-  bgr.velocityY = 3;
+  bgr.velocityY = 4;
   if(bgr.y>displayHeight){
   bgr.y = 200;
   }
@@ -133,10 +139,10 @@ if(a == 1){
     if(frameCount % 60 === 0 && a === 1)  {
       var meteors = createSprite(250,0,50,36)
       meteors.addImage(meteorimage)
-      meteors.debug = true
+      //meteors.debug = true
       meteors.scale = 0.1
       meteors.x = random(0,400);
-      meteors.setCollider("circle",0,0,40)
+      meteors.setCollider("circle",0,0,200)
       meteors.velocityY = 3
       meteorsGroup.add(meteors);
       if(player.isTouching(meteorsGroup)){
@@ -145,71 +151,65 @@ if(a == 1){
       }
      
     }
-    if(frameCount % 100 === 0 && a === 2)  {
+    if(frameCount % 100 === 0 && score>=4)  {
       var asteroids = createSprite(250,0,50,36)
       asteroids.addImage(asteroidimage)
-      asteroids.debug = true
-      asteroids.scale = 0.05
+      //asteroids.debug = true
+      asteroids.scale = 0.2
       asteroids.x = random(0,400);
-      asteroids.setCollider("circle",0,0,40)
+      asteroids.setCollider("circle",0,0,200)
       asteroids.velocityY = 5
       asteroidsGroup.add(asteroids);
       if(player.isTouching(asteroidsGroup)){
        player.destroy();
       b = 2;
       }
-     if(b === 2){
-       text("Rocket has been destroyed , please relaunch it",400,500)
-     }
+  
+    }
+
+
+    if(frameCount % 100 === 0 && score >= 6)  {
+      var alien = createSprite(250,0,50,36)
+      alien.addImage(alienimage)
+     // alien.debug = true
+      alien.scale = 0.1;
+      alien.x = random(0,400);
+      alien.setCollider("circle",0,0,600)
+      alien.velocityY = 5
+      alienGroup.add(alien);
+      if(player.isTouching(alienGroup)){
+       player.destroy();
+      b = 3;
+      }
+    
     }
     if(frameCount % 40 === 0 && a === 1)  {
       var power = createSprite(250,0,50,36)
-      power.addImage(powerimage);
-      power.scale= 0.4;
       power.x = random(0,400);
       power.velocityY = 3
       powerGroup.add(power)
       if(player.isTouching(powerGroup)){
         bgr.velocityY = bgr.velocityY + 9;
-        console.log(bgr.velocityY)
+        //console.log(bgr.velocityY)
        score = score + 2
       }
     
      
     }
 
- console.log(score) 
+ //console.log(score) 
  drawSprites();
  fill("white")
  text("Score"+ score,700,400)
- if(b === 1){
+ if(b === 1||b===2||b===3){
   text("Rocket has been destroyed , please relaunch it",400,500)
   bgr.velocityY = 0;
 }
-if(score === 15){
-  a = 3
-}
-if(frameCount % 100 === 0 && a === 3)  {
-  var alien = createSprite(250,0,50,36)
-  alien.addImage(alienimage)
-  alien.debug = true
-  alien.scale = 4;
-  alien.x = random(0,400);
-  alien.setCollider("circle",0,0,40)
-  alien.velocityY = 5
-  alienGroup.add(alien);
-  if(player.isTouching(alienGroup)){
-   player.destroy();
-  b = 3;
-  }
- if(b === 3){
-   text("Rocket has been destroyed , please relaunch it",400,500)
- }
-}
+
+
 }
 
-if(gameState === "bhaskara2"){
- // image(bgrimage2,0,-displayHeight * 4,displayWidth,displayHeight * 5)
+/*if(gameState === "bhaskara2"){
   background(100,250,35)
   player.debug = true;
   player.setCollider("rectangle",0,0,200,450)
@@ -217,8 +217,8 @@ if(gameState === "bhaskara2"){
   rocket2.destroy();
   input.hide();
   startrocket.visible = true
-  player.addImage("player",rocket1image)
-  player.scale = 0.3;
+  player.addImage("player2",rocket2image)
+  player.scale = 0.4;
   textSize(20)
   text("This is bhaskara 2",900,600)
   if(mousePressedOver(startrocket)){
@@ -226,14 +226,12 @@ if(gameState === "bhaskara2"){
     fill("white")
     text("Score"+ score,700,400)
     bgr.scale = 4;
-    /*player.velocityY = -5;
-    player.velocityX = 0;*/
     a = 1
     startrocket.destroy();
     
   }
-  
-if(a == 1){
+  //console.log(a);
+if(a === 1){
   bgr.velocityY = 3;
   if(bgr.y>displayHeight){
   bgr.y = 400;
@@ -258,22 +256,23 @@ if(a == 1){
       var meteors = createSprite(250,0,50,36)
       meteors.addImage(meteorimage)
       meteors.debug = true
-      meteors.scale = 0.05
+      meteors.scale = 0.2
       meteors.x = random(0,400);
-      meteors.setCollider("circle",0,0,400)
+      meteors.setCollider("circle",0,0,200)
       meteors.velocityY = 3
-      meteorsGroup.add(meteors);
-      if(meteorsGroup.isTouching(player)){
-       player.destroy();
-       //player.visible = false;
-      b = 1;
+      meteorsGroup2.add(meteors);
+      if(player.isTouching(meteorsGroup2)){
+        console.log("hey")
+  b=1
       }
+
      if(b === 1){
        text("Rocket has been destroyed , please relaunch it",400,500)
+       console.log("hi")
      }
     }
 
-    if(frameCount % 100 === 0 && a === 2)  {
+    if(frameCount % 100 === 0 && score>= 4)  {
       var asteroids = createSprite(250,0,50,36)
       asteroids.addImage(asteroidimage)
       asteroids.debug = true
@@ -291,7 +290,7 @@ if(a == 1){
      }
     }
 
-    if(frameCount % 100 === 0 && a === 3)  {
+    if(frameCount % 100 === 0 && score>=6)  {
       var alien = createSprite(250,0,50,36)
       alien.addImage(alienimage)
       alien.debug = true
@@ -315,18 +314,18 @@ if(a == 1){
       powerGroup.add(power);
       if(player.isTouching(powerGroup)){
         bgr.velocityY = bgr.velocityY + 99;
-        console.log(bgr.velocityY)
+        //console.log(bgr.velocityY)
        score = score + 2
       }
     
      
     }
     
- console.log(score) 
+ //console.log(score) 
  drawSprites(); 
  fill("white")
  text("Score"+ score,700,400) 
-}
+}*/
 
 
   
